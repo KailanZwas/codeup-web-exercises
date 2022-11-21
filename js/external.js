479b3487d8a1b2f276185a03743c82e0341a65bd
Kailan Zwaschka <kailan.zwaschka@gmail.com>
2022-11-21 13:41:23 -0600

    created inline/external

---
+++ js/external.js
@@ -0,0 +1,10 @@
+console.log("Hello from external JavaScript");
+
+// alert Welcome message
+
+alert('Welcome to my Website!');
+
+// alert favorite color
+var favColor = prompt('What is your favorite color?');
+
+alert("I love " + favColor + " too!");
