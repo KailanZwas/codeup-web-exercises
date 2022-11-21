4064260d343e1ddaf57335de3ec459c038f7a535
Kailan Zwaschka <kailan.zwaschka@gmail.com>
2022-11-21 16:52:43 -0600

    functions homework

---
+++ js/functions-lesson.js
@@ -0,0 +1,50 @@
+// define a function and immediately invoke (i.e. call) it
+(function () {
+
+
+
+myFunction();
+
+//let userName = prompt("What is your name?")
+//let message = myFunction("Kailan");
+//console.log(message);
+
+let person1 = "Billy",
+    person2 = "Bob";
+fight("Billy", "Bob");
+let x = isEven(543453464345634);
+console.log(x);
+let y = isEven(34563948576934576);
+console.log(x);
+let iBreakThings = isEven("true");
+console.log(iBreakThings)
+
+function myFunction(name) {
+
+    return " Thank you, " + name + ". You have ran my function";
+
+    //console.log("You are running myFunction");
+}
+
+
+function fight(person1, person2){
+    console.log(person1 + " punched " + person2 + " right in the kisser.")
+
+
+    //Get the pieces of the puzzle!!!!
+    //Put the pieces of the puzzle into variables
+
+    let number = num;
+    let answer = (number % 2 == 0);
+
+//function isEven(num){
+    //return (num % 2 == 0)
+
+    //Finially return the answer
+    return answer;
+}
+
+
+
+
+})();  /// Tail of IIFE
