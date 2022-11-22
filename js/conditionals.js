0db40c93550e4102756cea140bc5e29dddf3352a
Kailan Zwaschka <kailan.zwaschka@gmail.com>
2022-11-22 14:20:57 -0600

    if else

---
+++ js/conditionals.js
@@ -0,0 +1,35 @@
+// let city = prompt("what city do you live in?");
+//
+// if (city == `Boston`){
+//     console.log(`The location is Boston.`);
+//     console.log(`Go Red Sox!`);
+// } else if (city === `San Antonio`) {
+//     console.log(`The location is San Antonio`);
+//     console.log("Go Spurs Go!");
+// } else if (city == city){
+//     console.log(`The loction is ${city}.`);
+//     console.log("Go... ${city}...Go!")
+// } else {
+//     console.log(`We don't know where this is.`);
+// }
+// console.log(addNumber("Hi"));
+// addNumber("5");
+// function addNumber(num1) {
+//     let number;
+//     if (typeof num1 == "string") {
+//         return"This is not a number stupid";
+//         //number = parseInt(num1);
+//     }
+//     else {
+//     return num1 + 2;
+// }
+
+let pizzaPreference = prompt("WHat is your favorite kind of pizza?");
+if ( pizzaPreference == "cheese") {
+    console.log("Thanks Kevin from Home Alone!")
+} else if (pizzaPreference == "Pineapples and hot sauce") {
+    console.log("What a coincidence, I like  pinaples and hot sauce too!");
+}
+else {
+    console.log("Oh, okay... that's cool... my aunt's vegan too...");
+}
