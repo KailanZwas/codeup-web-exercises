31c57c1426ba759f5fcb03a8fc9a0de10358854e
Kailan Zwaschka <kailan.zwaschka@gmail.com>
2022-11-22 17:55:10 -0600

    conditionals homework

---
+++ js/conditional-lesson.js
@@ -0,0 +1,96 @@
+// let city = prompt("what city do you live in?");
+//
+// if (city == `Boston`){
+//     console.log(`The location is Boston.`);
+//     console.log(`Go Red Sox!`);
+// } else if (city === `San Antonio`) {
+//     console.log(`The location is San Antonio`);
+//     console.log("Go Spurs Go!");
+// } else if (city == city){
+//     console.log(`The loction is ${city}.`);
+//     console.log("Go... ${city}...Go!")
+// } else {
+//     console.log(`We don't know where this is.`);
+// }
+// console.log(addNumber("Hi"));
+// addNumber("5");
+// function addNumber(num1) {
+//     let number;
+//     if (typeof num1 == "string") {
+//         return"This is not a number stupid";
+//         //number = parseInt(num1);
+//     }
+//     else {
+//     return num1 + 2;
+// }
+
+ let pizzaPreference = prompt("WHat is your favorite kind of pizza?");
+// //if ( pizzaPreference.toLocaleLowerCase() == "cheese") {
+// if (pizzaPreference == 'cheese' || pizzaPreference == 'pepperoni') {
+//     console.log("Thanks Kevin from Home Alone!")
+// } else if (pizzaPreference == "Pineapples and hot sauce") {
+//     console.log("What a coincidence, I like  pineapples and hot sauce too!");
+// }
+// else {
+//     console.log("Oh, okay... that's cool... my aunt's vegan too...");
+// }
+
+
+
+// switch(pizzaPreference){
+//     case "cheese":
+//         console.log("Thanks Kevin from Home Alone!");
+//         //break;
+//     case "pineapples and hot sauce":
+//         console.log("What a coincidence, I like  pineapples and hot sauce too!");
+//         break;
+//     default:
+//         console.log("Oh, okay... that's cool... my aunt's vegan too...");
+//
+//
+// }
+
+// Determine if the4 variable is between 5 and 8
+let number = 1;
+if(number < 5) {
+    console.log('the number came before 5');
+}else if (number > 4 && number < 9){
+    console.log("the number came between 5 and 8");
+}
+else {
+    console.log("The number is greater then 8");
+}
+//
+// switch (number){
+//     case 1:
+//         console.log(`the number is ${number}. but it will fall through case 1`);
+//     case 2:
+//         console.log(`the number is ${number}. but it will fall through case 2`);
+//     case 3:
+//         console.log(`the number is {number}. but it will fall through case 3`);
+//     case 4:
+//         console.log("the number came before 5, and stopped and case 4 because of break");
+//         break;
+//     case 5:
+//     case 6:
+//     case 7:
+//     case 8:
+//         console.log("The number came between 5 and 8");
+//         break;
+//         console.log("the number came after 8");
+// }
+
+
+
+
+
+// let success = true;
+// let message;
+// if(success) {
+//     message = "The operatioin was succesful";
+// }
+// else {
+//     message = "The operation was not succesful";
+// }
+// let message = success ? "The operation was succesful" : "The operaiton was not succesful"
+// console.log(message);
--- js/conditionals.js
+++ js/conditionals.js
@@ -1,96 +1,129 @@
-// let city = prompt("what city do you live in?");
-//
-// if (city == `Boston`){
-//     console.log(`The location is Boston.`);
-//     console.log(`Go Red Sox!`);
-// } else if (city === `San Antonio`) {
-//     console.log(`The location is San Antonio`);
-//     console.log("Go Spurs Go!");
-// } else if (city == city){
-//     console.log(`The loction is ${city}.`);
-//     console.log("Go... ${city}...Go!")
-// } else {
-//     console.log(`We don't know where this is.`);
-// }
-// console.log(addNumber("Hi"));
-// addNumber("5");
-// function addNumber(num1) {
-//     let number;
-//     if (typeof num1 == "string") {
-//         return"This is not a number stupid";
-//         //number = parseInt(num1);
-//     }
-//     else {
-//     return num1 + 2;
-// }
+"use strict";
 
- let pizzaPreference = prompt("WHat is your favorite kind of pizza?");
-// //if ( pizzaPreference.toLocaleLowerCase() == "cheese") {
-// if (pizzaPreference == 'cheese' || pizzaPreference == 'pepperoni') {
-//     console.log("Thanks Kevin from Home Alone!")
-// } else if (pizzaPreference == "Pineapples and hot sauce") {
-//     console.log("What a coincidence, I like  pineapples and hot sauce too!");
-// }
-// else {
-//     console.log("Oh, okay... that's cool... my aunt's vegan too...");
-// }
+/* ########################################################################## */
 
-
-
-// switch(pizzaPreference){
-//     case "cheese":
-//         console.log("Thanks Kevin from Home Alone!");
-//         //break;
-//     case "pineapples and hot sauce":
-//         console.log("What a coincidence, I like  pineapples and hot sauce too!");
-//         break;
-//     default:
-//         console.log("Oh, okay... that's cool... my aunt's vegan too...");
-//
-//
-// }
-
-// Determine if the4 variable is between 5 and 8
-let number = 1;
-if(number < 5) {
-    console.log('the number came before 5');
-}else if (number > 4 && number < 9){
-    console.log("the number came between 5 and 8");
+/**
+ * TODO:
+ * Create a function named `analyzeColor` that accepts a string that is a color
+ * name as input. This function should return a message which relates to the
+ * color stated in the argument of the function. For colors you do not have
+ * responses written for, return a string stating so
+ *
+ * Example:
+ *  > analyzeColor('blue') // returns "blue is the color of the sky"
+ *  > analyzeColor('red') // returns "Strawberries are red"
+ *
+ *
+ *  > analyzeColor('cyan') // returns "I don't know anything about cyan"
+ *
+ * You should use an if-else-if-else block to return different messages.
+ *
+ * Test your function by passing various string literals to it and
+ * console.logging the function's return value
+ */
+let userPrompt = prompt("what is your favorite color");
+let message = analyzeColor(userPrompt);
+console.log(message);
+// let message = analyzeColor("yellow");
+//     console.log(message);
+// let message2 = analyzeColor("blue");
+//     console.log(message2);
+function analyzeColor(color) {
+    if (color === "yellow")  {
+        return("Yellow is the color of the sun.");
+    } else if (color === "blue") {
+        return("Blue is the color of the sky");
+    } else {
+        return ("we dont know what that color is.");
+    }
 }
-else {
-    console.log("The number is greater then 8");
-}
-//
-// switch (number){
-//     case 1:
-//         console.log(`the number is ${number}. but it will fall through case 1`);
-//     case 2:
-//         console.log(`the number is ${number}. but it will fall through case 2`);
-//     case 3:
-//         console.log(`the number is {number}. but it will fall through case 3`);
-//     case 4:
-//         console.log("the number came before 5, and stopped and case 4 because of break");
-//         break;
-//     case 5:
-//     case 6:
-//     case 7:
-//     case 8:
-//         console.log("The number came between 5 and 8");
-//         break;
-//         console.log("the number came after 8");
+//     if (analyzeColor ("yellow"));
+//     console.log("Yellow is the color of the sun!")
+// } else if (analyzeColor("blue")) {
+//     console.log(analyzeColor("blue is the color of the sky.");
+// } else if (analyzeColor("red")) {
+//     console.log("Cardinal is red");
+// } else if (analyzeColor("green")) {
+//     console.log("the grass is green");
 // }
 
+console.log();
 
 
 
+// Don't change the next two lines!
+// These lines create two variables for you:
+// - `colors`: a list of the colors of the rainbow
+// - `randomColor`: contains a single random color value from the list (this
+//                  will contain a different color every time the page loads)
+var colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+var randomColor = colors[Math.floor(Math.random() * colors.length)];
+/**
+ * TODO:
+ * Pass the `randomColor` variable to your 'analyzeColor' function and console.log the results.
+ * You should see a different message every time you refresh the page
+ */
 
-// let success = true;
-// let message;
-// if(success) {
-//     message = "The operatioin was succesful";
-// }
-// else {
-//     message = "The operation was not succesful";
-// }
-// let message = success ? "The operation was succesful" : "The operaiton was not succesful"
-// console.log(message);
+/**
+ * TODO:
+ * Comment out the code above, and refactor your function to use a switch-case statement
+ */
+
+/**
+ * TODO:
+ * Prompt the user for a color when the page loads, and pass the input from the
+ * user to your `analyzeColor` function. Alert the return value from your
+ * function to show it to the user.
+ */
+
+/* ########################################################################## */
+
+/**
+ * TODO:
+ * Suppose there's a promotion in Walmart, each customer is given a randomly
+ * generated "lucky number" between 0 and 5. If your lucky number is 0 you have
+ * no discount, if your lucky number is 1 you'll get a 10% discount, if it's 2,
+ * the discount is 25%, if it's 3, 35%, if it's 4, 50%, and if it's 5 you'll get
+ * everything for free!.
+ *
+ * Write a function named `calculateTotal` which accepts a lucky number and total
+ * amount, and returns the discounted price.
+ *
+ * Example:
+ * calculateTotal(0, 100) // returns 100
+ * calculateTotal(4, 100) // returns 50
+ * calculateTotal(5, 100) // returns 0
+ *
+ * Test your function by passing it various values and checking for the expected
+ * return value.
+ */
+
+/**
+ * TODO:
+ * Uncomment the line below to generate a random number between 0 and 5.
+ * (In this line of code, 0 is inclusive, and 6 is exclusive)
+ * Prompt the user for their total bill, then use your `calculateTotal` function
+ * and alerts to display to the user what their lucky number was, what their
+ * price before the discount was, and what their price after the discount is.
+ */
+// Generate a random number between 0 and 6
+// var luckyNumber = Math.floor(Math.random() * 6);
+
+/**
+ * TODO:
+ * Write some JavaScript that uses a `confirm` dialog to ask the user if they
+ * would like to enter a number. If they click 'Ok', prompt the user for a
+ * number, then use 3 separate alerts to tell the user:
+ *
+ * - whether the number is even or odd
+ * - what the number plus 100 is
+ * - if the number is negative or positive
+ *
+ * Do *NOT* display any of the above information
+ * if the user enters a value that is not of the number data type.
+ * Instead, use an alert to inform them of the incorrect input data type.
+ *
+ *
+ * Can you refactor your code to use functions?
+ * HINT: The way we prompt for a value could be improved
+ */
