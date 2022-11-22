610380ad8970e45be99ed7b2aeda379c2787b823
Kailan Zwaschka <kailan.zwaschka@gmail.com>
2022-11-22 14:41:58 -0600

    ternary statment

--- js/conditionals.js
+++ js/conditionals.js
@@ -24,12 +24,24 @@
 //     return num1 + 2;
 // }
 
-let pizzaPreference = prompt("WHat is your favorite kind of pizza?");
-if ( pizzaPreference == "cheese") {
-    console.log("Thanks Kevin from Home Alone!")
-} else if (pizzaPreference == "Pineapples and hot sauce") {
-    console.log("What a coincidence, I like  pinaples and hot sauce too!");
-}
-else {
-    console.log("Oh, okay... that's cool... my aunt's vegan too...");
-}
+// let pizzaPreference = prompt("WHat is your favorite kind of pizza?");
+// //if ( pizzaPreference.toLocaleLowerCase() == "cheese") {
+// if (pizzaPreference == 'cheese' || pizzaPreference == 'pepperoni') {
+//     console.log("Thanks Kevin from Home Alone!")
+// } else if (pizzaPreference == "Pineapples and hot sauce") {
+//     console.log("What a coincidence, I like  pineapples and hot sauce too!");
+// }
+// else {
+//     console.log("Oh, okay... that's cool... my aunt's vegan too...");
+// }
+
+let success = true;
+// let message;
+// if(success) {
+//     message = "The operatioin was succesful";
+// }
+// else {
+//     message = "The operation was not succesful";
+// }
+let message = success ? "The operation was succesful" : "The operaiton was not succesful"
+console.log(message);
