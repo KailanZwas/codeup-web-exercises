b2b74f23300cec2ce5be9dc077c0fd4934671e42
Kailan Zwaschka <kailan.zwaschka@gmail.com>
2022-11-22 15:28:40 -0600

    conditionals lessons class complete

--- js/conditionals.js
+++ js/conditionals.js
@@ -37,19 +37,48 @@
 
 
 
-switch(pizzaPreference){
-    case "cheese":
-        console.log("Thanks Kevin from Home Alone!");
-        break;
-    case "pineapples and hot sauce":
-        console.log("What a coincidence, I like  pineapples and hot sauce too!");
-        break;
-    default:
-        console.log("Oh, okay... that's cool... my aunt's vegan too...");
+// switch(pizzaPreference){
+//     case "cheese":
+//         console.log("Thanks Kevin from Home Alone!");
+//         //break;
+//     case "pineapples and hot sauce":
+//         console.log("What a coincidence, I like  pineapples and hot sauce too!");
+//         break;
+//     default:
+//         console.log("Oh, okay... that's cool... my aunt's vegan too...");
+//
+//
+// }
 
-
+// Determine if the4 variable is between 5 and 8
+let number = 1;
+if(number < 5) {
+    console.log('the number came before 5');
+}else if (number > 4 && number < 9){
+    console.log("the number came between 5 and 8");
 }
-
+else {
+    console.log("The number is greater then 8");
+}
+//
+// switch (number){
+//     case 1:
+//         console.log(`the number is ${number}. but it will fall through case 1`);
+//     case 2:
+//         console.log(`the number is ${number}. but it will fall through case 2`);
+//     case 3:
+//         console.log(`the number is {number}. but it will fall through case 3`);
+//     case 4:
+//         console.log("the number came before 5, and stopped and case 4 because of break");
+//         break;
+//     case 5:
+//     case 6:
+//     case 7:
+//     case 8:
+//         console.log("The number came between 5 and 8");
+//         break;
+//         console.log("the number came after 8");
+// }
 
 
 
