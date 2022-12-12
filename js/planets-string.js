2de35a999c8a855e23a3cee93acaa8111f7a1cf1
Kailan Zwaschka <kailan.zwaschka@gmail.com>
2022-12-12 13:47:47 -0600

    move line of code from bonuse to todo

--- js/planets-string.js
+++ js/planets-string.js
@@ -2,7 +2,7 @@
     "use strict";
 
     var planetsString = "Mercury|Venus|Earth|Mars|Jupiter|Saturn|Uranus|Neptune";
-   // var planetsArray = planetsString.split("|");
+
 
     /**
      * TODO:
@@ -10,22 +10,23 @@
      * planetsArray.
      * console.log planetsArray to check your work
      */
-
+    // var planetsArray = planetsString.split("|");
     // console.log(planetsArray);
 
     /**
      * TODO:
      * Create a string with <br> tags between each planet. console.log() your
      * results. Why might this be useful?
-     *
-     * BONUS:
-     * Create another string that would display your planets in an undordered
-     * list. You will need an opening AND closing <ul> tags around the entire
-     * string, and <li> tags around each planet.
      */
+
     var planetsArray = planetsString.split("|");
     let platenLineBreak = planetsString.split("|").join("<br>");
     console.log(platenLineBreak);
 
+     /** BONUS:
+     * Create another string that would display your planets in an undordered
+     * list. You will need an opening AND closing <ul> tags around the entire
+     * string, and <li> tags around each planet.
+     */
 
 })();
