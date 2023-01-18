5dde89d820eeb7ee97964377cb983cf619a01496
Kailan Zwaschka <kailan.zwaschka@gmail.com>
2023-01-18 10:17:42 -0600

    konami code

--- js/warm-ups.js
+++ js/warm-ups.js
@@ -291,48 +291,48 @@
 //     return outPutArray;
 //
 // }
-
-================================= WARM UP
-
-Write a function, getUserCredentials, that takes in a list of user objects and returns a list of user objects with
-    only the username and password properties. Assume at least an array of one user object.
-
-
-    const users = [
-    {
-        firstName: 'Justin',
-        lastName: 'Reich',
-        dob: '1923-01-01',
-        username: 'jreich',
-        password: '$2y$10$UJlsa5vWq5DUKJjyO38gM.dCZfudWOFCrLWQosh0mhXKaZmRmvDse'
-    },
-    {
-        firstName: 'Sally',
-        lastName: 'Smith',
-        dob: '1935-03-11',
-        username: 'ssmith',
-        password: '$2y$10$VaLGU5.7uQLr.eg6kSI9seOcP4JY4XktWt28I9JgblAGIDpkDXbya'
-    },
-    {
-        firstName: 'Fred',
-        lastName: 'Smith',
-        dob: '1999-01-21',
-        username: 'fsmith',
-        password: '$2y$10$3USt6Dl8TNMkeh0KioPnfeVpynAotXvSIJ5xrzAHragPEAWMYEBNS'
-    },
-]
-
-function getUserCredentials(users) {
-    const credentials = [];
-    for (let i = 0; i < users.length; i++) {
-        const user = users[i];
-        credentials.push({ username: user.username, password: user.password });
-    }
-    return credentials;
-}
-
-
-getUserCredentials(users) // returns...
+//
+// ================================= WARM UP
+//
+// Write a function, getUserCredentials, that takes in a list of user objects and returns a list of user objects with
+//     only the username and password properties. Assume at least an array of one user object.
+//
+//
+//     const users = [
+//     {
+//         firstName: 'Justin',
+//         lastName: 'Reich',
+//         dob: '1923-01-01',
+//         username: 'jreich',
+//         password: '$2y$10$UJlsa5vWq5DUKJjyO38gM.dCZfudWOFCrLWQosh0mhXKaZmRmvDse'
+//     },
+//     {
+//         firstName: 'Sally',
+//         lastName: 'Smith',
+//         dob: '1935-03-11',
+//         username: 'ssmith',
+//         password: '$2y$10$VaLGU5.7uQLr.eg6kSI9seOcP4JY4XktWt28I9JgblAGIDpkDXbya'
+//     },
+//     {
+//         firstName: 'Fred',
+//         lastName: 'Smith',
+//         dob: '1999-01-21',
+//         username: 'fsmith',
+//         password: '$2y$10$3USt6Dl8TNMkeh0KioPnfeVpynAotXvSIJ5xrzAHragPEAWMYEBNS'
+//     },
+// ]
+//
+// function getUserCredentials(users) {
+//     const credentials = [];
+//     for (let i = 0; i < users.length; i++) {
+//         const user = users[i];
+//         credentials.push({ username: user.username, password: user.password });
+//     }
+//     return credentials;
+// }
+//
+//
+// getUserCredentials(users) // returns...
 
 /*
 
@@ -352,3 +352,78 @@ getUserCredentials(users) // returns...
 ]
 
 */
+
+
+
+// ================================= WARM UP
+
+// Write a function that takes a neighborhood object and determines if it is desirable. A neighborhood is desirable
+// if the median home price is less than 300000, crime rates are low, and the total rating of schools is at least 24.
+
+
+// example data...
+
+const neighborhood1 = {
+    neighborhood: "Lovely Estates",
+    medianHomePrice: 280000,
+    pool: true,
+    tennis: false,
+    crimeRate: "low",
+    schools: [
+        {name: "Elementary School", rating: 8},
+        {name: "Middle School", rating: 6},
+        {name: "High School", rating: 8}
+    ]
+};
+
+const neighborhood2 = {
+    neighborhood: "Luminous Estates",
+    medianHomePrice: 270000,
+    pool: true,
+    tennis: false,
+    crimeRate: "high",
+    schools: [
+        {name: "Elementary School", rating: 8},
+        {name: "Middle School", rating: 8},
+        {name: "High School", rating: 8}
+    ]
+}
+
+const neighborhood3 = {
+    neighborhood: "Oak Mountain",
+    medianHomePrice: 290000,
+    pool: false,
+    tennis: false,
+    crimeRate: "low",
+    schools: [
+        {name: "Elementary School", rating: 8},
+        {name: "Middle School", rating: 8},
+        {name: "High School", rating: 8}
+    ]
+}
+
+const neighborhood3 = {
+    neighborhood: "Ginormous Acres",
+    medianHomePrice: 350000,
+    pool: true,
+    tennis: true,
+    crimeRate: "low",
+    schools: [
+        {name: "Elementary School", rating: 9},
+        {name: "Middle School", rating: 9},
+        {name: "High School", rating: 9}
+    ]
+}
+
+fn(neighborhood1) // returns false due to school rating
+fn(neighborhood2) // returns false due to crime rate
+fn(neighborhood3) // returns true
+fn(neighborhood4) // returns false due to median home price
+
+
+function affordableSafeSmartHome(price, crime, schools) {
+
+}
+
+
+
